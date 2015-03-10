4050082e3c0098848c2a0929cbf0d61e4c2a321c
Xiao Meng <xiaom.cs@gmail.com>
2015-03-10 11:26:00 -0700

    Decompose hive

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURDIR=$(pwd)
 BASEDIR=$(cd $(dirname $0); pwd)"/.."
-dir_list=( "base" "zookeeper" "drill" "hbase" "drill-hive") 
+dir_list=( "base" "zookeeper" "drill" "hbase" "hive")
 
 # NOTE: the order matters but this is the right one
 for i in ${dir_list[@]}; do
