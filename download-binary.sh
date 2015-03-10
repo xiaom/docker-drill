71b746111429e746efb76e5c7f17f7b0323e1f95
Xiao Meng <xiaom.cs@gmail.com>
2015-03-10 10:53:22 -0700

    Add download scripts. Fix drill server configuration.

---
+++ download-binary.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+DRILL_VERSION=0.8.0
+HADOOP_VERSION=2.6.0
+HIVE_VERSION=0.13.0
+HBASE_VERSION=0.98.7
+ZOOKEEPER_VERSION=3.4.6
+curl -SsfLO "https://builds.apache.org/job/drill-scm/lastSuccessfulBuild/artifact/distribution/target/apache-drill-${DRILL_VERSION}-SNAPSHOT.tar.gz" 
+curl -SsfLO "http://www.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz" 
+curl -SsfLO "http://archive.apache.org/dist/hive/hive-$HIVE_VERSION/apache-hive-$HIVE_VERSION-bin.tar.gz"
+curl -SsfLO "http://archive.apache.org/dist/hbase/hbase-$HBASE_VERSION/hbase-$HBASE_VERSION.tar.gz"
+curl -SsfLO "http://www.apache.org/dist/zookeeper/zookeeper-${ZOOKEEPER_VERSION}/zookeeper-${ZOOKEEPER_VERSION}.tar.gz"
+
+mv apache-drill-${DRILL_VERSION}-SNAPSHOT.tar.gz  drill/files
+mv hadoop-$HADOOP_VERSION.tar.gz hive/files
+mv apache-hive-$HIVE_VERSION-bin.tar.gz hive/files
+mv hbase-$HBASE_VERSION.tar.gz hbase/files
+mv zookeeper-${ZOOKEEPER_VERSION}.tar.gz zookeeper/files
